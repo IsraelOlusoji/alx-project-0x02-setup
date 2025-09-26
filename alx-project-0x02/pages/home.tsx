24acc589368b4152a89bed0244fc82158fccf4fe
jungles001 <olusojiisraelife@gmail.com>
2025-09-26 20:53:38 +0100

    Basic Routing in Next.js

---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Welcome to alx-project-2 🚀</h1>
+    </div>
+  );
+};
+
+export default Home;
