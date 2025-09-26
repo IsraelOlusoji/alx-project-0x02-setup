36b7e02259890c6b3af9f3089c007d0a30716176
jungles001 <olusojiisraelife@gmail.com>
2025-09-26 23:20:34 +0100

    Build a UserCard Component

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -29,7 +29,7 @@ const Users = ({ users }: UsersPageProps) => {
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
+export async function getStaticProps() {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
 
